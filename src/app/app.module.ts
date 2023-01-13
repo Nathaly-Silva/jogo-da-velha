4aa7d6c9d71efc3f5118e7277e6d149b80441c7a
nathalyperetto <nathy_peretto@hotmail.com>
2023-01-13 14:05:13 -0300

    configuração inicial do jogo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { JogoDaVelhaModule } from './jogo-da-velha/jogo-da-velha.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    JogoDaVelhaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
